4d12dc516d9b99f5e638128005e5977205f0dbc5
IgorSh <ivsnew@mail.ru>
2021-10-17 12:15:10 +0300

    first

---
+++ main.py
@@ -0,0 +1,38 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from jira import JIRA
+
+loginJira = 'ivsnew@mail.ru'
+passJira = 'bujhm212'
+tokenJira = 'ArE9KsDPl4VihdtYfpt0287D'
+
+def initJira():
+    jira_options = {'server': 'https://open-genes.atlassian.net'}
+    return JIRA(options=jira_options, basic_auth=(loginJira, tokenJira))
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, i am {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('jiraPy')
+
+    jira = initJira()
+    project_key = 'OG'
+    work_date = '20211001'
+    reporter = '70121:3bf008e2-6e0d-4025-9572-24942d2fee45'
+    jql = 'project = ' + project_key + ' AND  worklogDate >= ' + work_date + ' AND  reporter = ' + reporter
+    issues_list = jira.search_issues(jql)
+
+    print(issues_list)
+    print(issues_list.total)
+    for issue in issues_list:
+        print("{0}".format(issue.key))
+        worklogs = jira.worklogs(issue.key)
+        print(worklogs)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
